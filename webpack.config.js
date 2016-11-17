5e6eb2e6f3fc2ffd4c9349ff01f3d0033c39d8d7
Kirill Stepkin <kirillstyopkin@gmail.com>
2016-11-17 19:24:17 +0200

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,42 @@ var autoprefixer = require('autoprefixer');
 var precss       = require('precss');
 var mixins       = require('postcss-mixins');
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 var appContext = path.join(__dirname, '/');
+
+var NODE_ENV = process.env.NODE_ENV || "production";
+console.log(NODE_ENV)
 
 module.exports = {
   context: appContext,
-  entry: './src/Chartify.js',
+  entry: [
+    './js/index',
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080/'
+  ],
   output: {
-    filename: "./chartify.js",
-    sourceMapFilename: './chartify.js.map',
-    library: 'Chartify',
-    libraryTarget: 'umd'
-  },
-  externals: {
-    'react': {
-      'commonjs': 'react',
-      'commonjs2': 'react',
-      'amd': 'react',
-      // React dep should be available as window.React, not window.react
-      'root': 'React'
-    }
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
   },
   module: {
     loaders: [{
       test: /\.js?$/,
       loaders: ['babel-loader?stage=0&optional=runtime'],
       exclude: /node_modules/,
-      include: path.join(__dirname, '/src')
+      include: path.join(__dirname, '/js')
     }, {
       test: /\.css?$/,
-      loader: 'style-loader!css-loader'
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!sass-loader')
     }, {
       test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
       loader: 'file?name=[path][name].[ext]?[hash]'
     }]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin("[name].css", { allChunks: true })
   ],
   postcss: function () {
     return [autoprefixer, precss, mixins];
